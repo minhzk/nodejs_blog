5a74fe23282e5f19e23aecdcd223ed905283e93d
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2023-10-14 21:47:25 +0700

    i morgan, add template engine, static files & SASS and add Bootstrap 4

---
+++ blog/src/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const {engine} = require('express-handlebars');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine setup
+app.engine('.hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+// console.log('PATH:', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
