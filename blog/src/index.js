ecb4abc0e58bb832738eb039b2e0fdbc3ffa44b4
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2023-10-15 22:56:30 +0700

    refactor: route news and site get method, resource path

--- blog/src/index.js
+++ blog/src/index.js
@@ -33,7 +33,7 @@ app.engine(
     }),
 );
 app.set('view engine', '.hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 // console.log('PATH:', path.join(__dirname, 'resources/views'));
 
 // Home, search, contact
@@ -42,6 +42,6 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
 
