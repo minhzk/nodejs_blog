7250a8142acb4d4699046bd014154934be93bf59
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2023-10-16 21:28:29 +0700

    feat: CRUD Create course

--- blog/src/app/models/Course.js
+++ blog/src/app/models/Course.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
+var slug = require('mongoose-slug-updater');
+
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    name: { type: String, maxLength: 255 },
-    description: { type: String, maxLength: 600 },
-    image: { type: String, maxLength: 255 },
-    slug: {type: String, maxLength: 255},
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+    name: { type: String, required: true },
+    description: { type: String },
+    image: { type: String },
+    slug: { type: String, slug: 'name', unique: true },
+    videoId: { type: String, required: true },
+    level: { type: String },
+}, { 
+    timestamps: true ,
 });
 
 module.exports = mongoose.model('Course', Course);
