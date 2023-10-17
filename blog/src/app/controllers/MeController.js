3eae45332d6f27d28b575189a85371c81028a4bc
Nguyễn Bình Minh <nbinhminh158@gmail.com>
2023-10-17 13:02:07 +0700

    feat: CRUD Update course

---
+++ blog/src/app/controllers/MeController.js
@@ -0,0 +1,16 @@
+const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mangoose');
+
+class MeController {
+
+    // [GET] /me/stored/courses
+    storedCourses(req, res, next) {
+        Course.find({})
+            .then(courses => res.render('me/stored-courses', {
+                courses: multipleMongooseToObject(courses)
+            }))
+            .catch(next);
+    }
+}
+
+module.exports = new MeController();
--- blog/src/index.js
+++ blog/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const path = require('path');
 
 const app = express();
@@ -22,6 +23,8 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'))
+
 // HTTP logger
 // app.use(morgan('combined'));
 
@@ -30,7 +33,11 @@ app.engine(
     '.hbs',
     engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        }
     }),
+    
 );
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
